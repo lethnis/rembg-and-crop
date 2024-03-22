e668515d9621ac3a3938afc28dae7d2a3ee4e0b6
arka-windows11 <arkad.sh@yandex.ru>
2024-03-22 10:44:33 +0700

    Added comments and example image

--- main.py
+++ main.py
@@ -5,38 +5,59 @@ from rembg import remove
 
 from utils import crop, prepare_to_save
 
-st.set_page_config(page_title="Remover", layout="wide", page_icon=":gem:")
+def main():
+    """Program takes an image, removes background and crops empty edges"""
 
-st.sidebar.write("Upload and download :gear:")
+    st.set_page_config(page_title="Удалятор 3000", layout="wide", page_icon=":robot_face:")
 
-img = st.sidebar.file_uploader("Pick an image", type=["jpg", "png"], accept_multiple_files=False)
+    st.sidebar.write("Загрузить и сохранить :gear:")
 
-col1, col2, col3 = st.columns(3)
+    upload = st.sidebar.file_uploader("Выберите изображение", type=["jpg", "png"], accept_multiple_files=False)
 
+    # if some image uploaded do logic on that image, else use default image
+    if upload:
+        img_name = upload.name
+        img = Image.open(upload)
+        remove_and_crop(img, img_name)
+    else:
+        img = Image.open("data/cat.jpg")
+        remove_and_crop(img, "cat.jpg")
+        
 
-if img:
-    img_name = img.name
-    img = Image.open(img)
-    col1.image(img, "Base image")
+def remove_and_crop(img, img_name):
+    """Makes 3 columns with original image, no background image and cropped image.
+    Adds buttons to download results"""
 
-    with st.spinner("Removing background :scissors:..."):
+    col1, col2, col3 = st.columns(3)
+
+    # Displays original image
+    col1.image(img, "Исходное изображение")
+
+    # Shows spinner while background removing, then displays new image
+    with st.spinner(":scissors: удалаем фон..."):
         no_bg = remove(img)
-    col2.image(no_bg, "Image with no background")
+    col2.image(no_bg, "Изображение без фона")
 
-    with st.spinner("Cropping empty edges :scissors:..."):
+    # Shows spinner while cropping, then displays new image
+    with st.spinner(":scissors: обрезаем края..."):
         cropped = crop(no_bg)
-    col3.image(cropped, "Cropped image")
+    col3.image(cropped, "Обрезанное изображение")
 
+    # Adds no background image download button
     st.sidebar.download_button(
-        label="Download without background",
+        label="Скачать без фона",
         data=prepare_to_save(no_bg),
         file_name="no_bg_" + img_name,
         mime="image/png",
     )
 
+    # Adds cropped image download button
     st.sidebar.download_button(
-        label="Download cropped",
+        label="Скачать обрезанное",
         data=prepare_to_save(cropped),
         file_name="cropped_" + img_name,
         mime="image/png",
     )
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,46 +1,63 @@
 import numpy as np
-from PIL import Image
+import PIL
 from io import BytesIO
 
 
 def get_coordinates(arr: np.array) -> tuple:
+    """Helper function for crop function. Takes an image as np.array and looks
+    through every side for non-empty lines"""
 
     height = arr.shape[0]
     width = arr.shape[1]
 
+    # looks from top to bottom until finds non-empty line
     for i in range(height):
         row = arr[i]
         if not (row == 0).all():
             upper = i
             break
 
+    # looks from bottom to top until finds non-empty line
     for i in range(height):
         row = arr[-i - 1]
         if not (row == 0).all():
             lower = height - i
             break
 
+    # looks from left to right until finds non-empty line
     for i in range(width):
         row = arr[:, i]
         if not (row == 0).all():
             left = i
             break
 
+    # looks from right to left until finds non-empty line
     for i in range(width):
         row = arr[:, -i - 1]
         if not (row == 0).all():
             right = width - i
             break
-
+    
+    # returns lines indexes in order that PIL requires
     return left, upper, right, lower
 
 
-def crop(img: Image) -> Image:
+def crop(img: PIL.Image) -> PIL.Image:
+    """Crop empty edges by coordinates"""
     arr = np.array(img)
     return img.crop(get_coordinates(arr))
 
 
-def prepare_to_save(img):
+def prepare_to_save(img: PIL.Image) -> BytesIO:
+    """Preparing image for saving"""
     buf = BytesIO()
     img.save(buf, format="PNG")
     return buf.getvalue()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
